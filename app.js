bf0d8f2a5eb19b23f509f30c040a4ca2e09c1ee9
MastanAbdulkhaligli <mastanabdulkhaligli@gmail.com>
2022-03-08 16:15:51 +0400

    Static Files Upload

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-const fn = () => {
-  console.log("Working");
-};
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "temp/index.html"));
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server has started on ${port}`);
+});
